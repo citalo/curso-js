b028ab804027d3a4fa8c881df4b115ac1b0f19d2
carlositalo.dev <carlositalo.dev@gmail.com>
2023-04-25 11:23:46 -0300

    curso de javascript

---
+++ Estrutura de repetição/While.js
@@ -0,0 +1,16 @@
+//while
+let number = 0;
+
+while (number <= 10) {
+   console.log (number);
+   number ++; //incrementa 1 a number, importante para o loop não ser infinito.
+}//importante colocar a condição que vá satisfazer o loop.
+console.log ("************")
+let number2 = 12;
+
+while (number2 > 0) {
+    console.log(number2);
+    number2--; //diminui 1 a cada loop
+}
+
+console.log("*************");//Combinando com estrutura de condições
---
+++ op. de comparacao.js
@@ -0,0 +1,35 @@
+console.log("..............");//igual
+console.log("10==11",10==11);
+console.log("10==10", 10==10);
+console.log("..............");//o javascript é case sensitive
+console.log("a==a","a"=="a");
+console.log("a==A","a"=="A");
+console.log("..............");//!= diferente
+console.log("10!=11",10!=11);
+console.log("10!=10",10!=10);
+console.log("a!=a","a"!="a");
+console.log("a!=A","a"!="A");
+console.log("..............");//=== estritamente igual
+console.log("10==10","10"==10); //igualdade analisa conteúdo
+console.log("10===10","10"===10); //estritamente igual analisa também o tipo.
+console.log("10===10", 10===10);
+console.log("..............");//!== estritamente diferente
+console.log("10!=10","10"!=10); 
+console.log("10!==10","10"!==10);//diferença no tipo da variável
+console.log("10!==10", 10!==10);
+console.log("..............");//> maior que
+console.log("10>09",10>09);
+console.log("10>10",10>10);
+console.log("10>11",10>11);
+console.log("..............");//< menor que
+console.log("10<09",10<09);
+console.log("10<10",10<09);
+console.log("10<11",10<11);
+console.log("..............");//>= maior ou igual que
+console.log("10>=09",10>=09);
+console.log("10>=10",10>=10);
+console.log("10>=11",10>=11);
+console.log("..............");//<= menor ou igual que
+console.log("10<=09",10<=09);
+console.log("10<=10",10<=10);
+console.log("10<=11",10<=11);
