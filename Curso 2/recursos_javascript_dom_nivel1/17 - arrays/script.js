f29ac78004601a554a940b68dfe8338f6549e32f
carlositalo.dev <carlositalo.dev@gmail.com>
2023-05-04 20:29:03 -0300

    atatualização do curso js

--- Curso 2/recursos_javascript_dom_nivel1/17 - arrays/script.js
+++ Curso 2/recursos_javascript_dom_nivel1/17 - arrays/script.js
@@ -4,57 +4,57 @@ let nomes = ["joao", "ana", "carlos"]
 // -------------------------------
 // alterar dados de um array
 // -------------------------------
-// nomes[1] = "maria"
-// console.table(nomes)
+ nomes[1] = "maria"
+console.table(nomes)
 
 // -------------------------------
 // quantos elementos tem um array
 // -------------------------------
-// console.log(nomes.length)
+console.log(nomes.length)
 
 // -------------------------------
 // adicionar mais elementos no final do array
 // -------------------------------
-// nomes.push("joaquim")
-// console.table(nomes)
+nomes.push("joaquim")
+console.table(nomes)
 
 // -------------------------------
 // adicionar elementos no início do array
 // -------------------------------
-// nomes.unshift("josé")
-// console.table(nomes)
+nomes.unshift("josé")
+console.table(nomes)
 
 // -------------------------------
 // remover elemento do início
 // -------------------------------
-// nomes.shift()
+// nomes.shift() // deixa apenas dois elementos finais na array
 // console.table(nomes)
 
 // -------------------------------
 // remover elemento do fim
 // -------------------------------
-// nomes.pop()
-// console.table(nomes)
+//nomes.pop() 
+//console.table(nomes)
 
 // -------------------------------
 // remover elementos permite guardar o valor removido numa variável
 // -------------------------------
-// let removido = nomes.pop()
-// console.log(removido)
-// console.table(nomes)
+let removido = nomes.pop()
+console.log(removido)
+console.table(nomes)
 
 // -------------------------------
 // remover e/ou adicionar elementos em qualquer posição dentro do array
 // -------------------------------
-// console.table(nomes)
-// nomes.splice(1,2,"joaquim", "manuel", "rui")
-// console.table(nomes)
+ console.table(nomes)
+ nomes.splice(1,2,"joaquim", "manuel", "rui")
+ console.table(nomes)
 
 
 // -------------------------------
 // ver eliminados
 // -------------------------------
-// console.table(nomes)
-// let eliminados = nomes.splice(1,2,"joaquim", "manuel", "rui")
-// console.table(nomes)
-// console.table(eliminados)
+console.table(nomes)
+let eliminados = nomes.splice(1,2,"joaquim", "manuel", "rui")
+console.table(nomes)
+console.table(eliminados)
