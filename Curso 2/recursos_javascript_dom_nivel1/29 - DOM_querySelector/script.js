f0799f1b714cd7222ec6e29311a3164ac6f67289
carlositalo.dev <carlositalo.dev@gmail.com>
2023-05-07 05:44:05 -0300

    atualização do curso de js

---
+++ Curso 2/recursos_javascript_dom_nivel1/29 - DOM_querySelector/script.js
@@ -0,0 +1,13 @@
+/*document.querySelector()
+document.querySelectorAll()
+*/
+//let e = document.querySelector("#titulo")//para id
+//console.log(e)
+
+//let x = document.querySelector(".cor-texto-noticia")//para class
+//console.log(e)
+
+//let y = document.querySelector("p")//para tag html - busca o 1º parágrafo.
+//console.log(e)
+
+//let z = document.querySelectorAll("p")// para tag html -busca todos os <p>
