d947494a7aa59aefd4cbddfd7746c77c26339971
carlositalo.dev <carlositalo.dev@gmail.com>
2023-05-25 20:41:36 -0300

    atualização do curso JS

---
+++ Curso 2/recursos_javascript_dom_nivel1/37 - DOM - Remover Event Listener/script.js
@@ -0,0 +1,19 @@
+/* 
+OBJETIVO:
+Ao clicar no botão, alterar a cor de fundo da caixa para aquamarine,
+apresentar o texto 'clique' na consola e
+remover o evento click do botão
+*/
+
+let caixa = document.querySelector(".caixa")//busca similar ao CSS
+let botao = document.querySelector ("button")
+
+function executar (event){
+    caixa.classList.add('cor-fundo-caixa')
+    console.log('clique')
+    botao.removeEventListener('click',executar)
+}
+
+botao.addEventListener('click',executar)
+
+
